99b80098c04afe0b4c7f87e8d73ddb6969399c5d
dhruv aggarwal <dhruvgwal@gmail.com>
2024-12-19 22:40:03 +0530

    starter

---
+++ src/components/cui/review.jsx
@@ -0,0 +1,86 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { CommentRatings } from "@/components/ui/rating";
+import { Card } from "../ui/card";
+
+export const Reviews = ({heading=""}) => {
+  const REVIEW_DATA = [
+    {
+      review:
+        "We had the most amazing time! Booking through MaltaXplore was easy and seamless. Highly recommend the sunset boat tour!",
+      name: "Sarah",
+      location: "United Kingdom",
+      avatar:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      rating: 4.5,
+    },
+    {
+      review:
+        "We had the most amazing time! Booking through MaltaXplore was easy and seamless. Highly recommend the sunset boat tour!",
+      name: "Sarah",
+      location: "United Kingdom",
+      avatar:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      rating: 4.5,
+    },
+    {
+      review:
+        "We had the most amazing time! Booking through MaltaXplore was easy and seamless. Highly recommend the sunset boat tour!",
+      name: "Sarah",
+      location: "United Kingdom",
+      avatar:
+        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      rating: 4.5,
+    },
+  ];
+  const ReviewCard = ({ data }) => (
+    <Card className="p-8 border-2 hover:border-primary transition-all ease-in-out">
+      <div className="flex items-center justify-between">
+        <Image src="images/el_quotes.svg" width={50} height={50} />
+
+        <CommentRatings rating={data.rating} />
+      </div>
+      <br />
+      <p>{data.review}</p>
+      <br />
+      <div className="flex items-center gap-4">
+        <Image
+          src={data.avatar}
+          className="rounded-full h-12 object-cover w-12"
+          width={50}
+          height={50}
+        />
+        <div>
+          <p className="text-xl font-semibold">{data.name}</p>
+          <p className="text-muted-foreground">{data.location}</p>
+        </div>
+      </div>
+    </Card>
+  );
+  return (
+    <div className="my-48 text-center px-32">
+      <div className="flex w-full justify-between">
+        <div></div>
+        <p className="text-5xl font-bold">{heading}</p>
+        <div className="flex gap-2 self-end items-center justify-end">
+          <Button variant="outline">
+            <ArrowLeftIcon />
+          </Button>
+          <Button variant="outline">
+            <ArrowRightIcon />
+          </Button>
+        </div>
+      </div>
+      <div className="grid text-left mt-16 grid-cols-3 gap-6">
+        {REVIEW_DATA.map((item) => (
+          <ReviewCard data={item} />
+        ))}
+      </div>
+      <br />
+      <Button size="lg" className="p-8">
+        Read More Reviews
+      </Button>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,24 +8,19 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 import {
-  ArrowLeftIcon,
-  ArrowRightIcon,
   CodeSandboxLogoIcon,
   Component1Icon,
   DotFilledIcon,
-  DotIcon,
   MagnifyingGlassIcon,
 } from "@radix-ui/react-icons";
 import { ChevronRight } from "lucide-react";
 import localFont from "next/font/local";
 import Image from "next/image";
 import { useState } from "react";
-import { useTranslation } from "react-i18next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -54,9 +49,8 @@ import {
   CarouselPrevious,
   CarouselProgress,
 } from "@/components/ui/zoom_carousel";
-import { Progress } from "@/components/ui/progress";
 import Tilt from "react-parallax-tilt";
-import { CommentRatings } from "@/components/ui/rating";
+import { Reviews } from "@/components/cui/review";
 
 const Categories = () => {
   const [date, setDate] = useState();
@@ -556,88 +550,6 @@ const MadeSimple = () => {
     </div>
   );
 };
-
-const Guests = () => {
-  const REVIEW_DATA = [
-    {
-      review:
-        "We had the most amazing time! Booking through MaltaXplore was easy and seamless. Highly recommend the sunset boat tour!",
-      name: "Sarah",
-      location: "United Kingdom",
-      avatar:
-        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      rating: 4.5,
-    },
-    {
-      review:
-        "We had the most amazing time! Booking through MaltaXplore was easy and seamless. Highly recommend the sunset boat tour!",
-      name: "Sarah",
-      location: "United Kingdom",
-      avatar:
-        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      rating: 4.5,
-    },
-    {
-      review:
-        "We had the most amazing time! Booking through MaltaXplore was easy and seamless. Highly recommend the sunset boat tour!",
-      name: "Sarah",
-      location: "United Kingdom",
-      avatar:
-        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      rating: 4.5,
-    },
-  ];
-  const ReviewCard = ({ data }) => (
-    <Card className="p-8 border-2 hover:border-primary transition-all ease-in-out">
-      <div className="flex items-center justify-between">
-        <Image src="images/el_quotes.svg" width={50} height={50} />
-
-        <CommentRatings rating={data.rating} />
-      </div>
-      <br />
-      <p>{data.review}</p>
-      <br />
-      <div className="flex items-center gap-4">
-        <Image
-          src={data.avatar}
-          className="rounded-full h-12 object-cover w-12"
-          width={50}
-          height={50}
-        />
-        <div>
-          <p className="text-xl font-semibold">{data.name}</p>
-          <p className="text-muted-foreground">{data.location}</p>
-        </div>
-      </div>
-    </Card>
-  );
-  return (
-    <div className="my-48 text-center px-32">
-      <div className="flex w-full justify-between">
-        <div></div>
-        <p className="text-5xl font-bold">What Our Guests Are Saying</p>
-        <div className="flex gap-2 self-end items-center justify-end">
-          <Button variant="outline">
-            <ArrowLeftIcon />
-          </Button>
-          <Button variant="outline">
-            <ArrowRightIcon />
-          </Button>
-        </div>
-      </div>
-      <div className="grid text-left mt-16 grid-cols-3 gap-6">
-        {REVIEW_DATA.map((item) => (
-          <ReviewCard data={item} />
-        ))}
-      </div>
-      <br />
-      <Button size="lg" className="p-8">
-        Read More Reviews
-      </Button>
-    </div>
-  );
-};
-
 const River = () => {
   return (
     <div className="px-32 relative">
@@ -760,7 +672,7 @@ export default function Home() {
       <Events />
       <MaltaPass />
       <MadeSimple />
-      <Guests />
+      <Reviews heading="What Our Guests Are Saying" />
       <River />
     </div>
   );

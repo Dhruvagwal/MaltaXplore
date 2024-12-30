cbcb5e5a31158a6b60afc1e2ca21fccae24ab69f
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2024-12-30 15:21:37 +0530

    fix responsiveness

--- src/pages/supplier-registration.jsx
+++ src/pages/supplier-registration.jsx
@@ -5,17 +5,16 @@ import React from "react";
 function SupplierRegistration() {
   return (
     <div className="from-primary-foreground bg-gradient-to-br to-transparent">
-      <Navbar />
-      <main className="pt-16 px-32">
+      <main className="pt-16 px-16 md:px-32">
         <p className="text-3xl font-bold text-center">
           Join Malta’s Leading Tourism Platform
         </p>
-        <Card className="mt-16 w-[50vw] mx-auto">
+        <Card className="mt-16 w-full md:w-[50vw] mx-auto">
           <CardHeader>
-            <p className="text-xl font-bold">Supplier Registration</p>
+            <p className="text-xl font-bold text-center">Supplier Registration</p>
           </CardHeader>
         </Card>
-        <div className="px-64 mt-16">
+        <div className="md:px-32 lg:px-64 mt-16">
           <p className="text-center">
             Our team will review your information and activate your account
             within 48 hours. Once approved, you’ll be able to list your

ca34a9b9fd621af7d0729e89b52d547a3a3e9072
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-11 14:08:48 +0530

    changes in table style

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -48,7 +48,7 @@ const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
-      "h-10 px-2 text-left border-r align-middle font-bold text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] p-4",
+      "h-10 text-left border-r align-middle font-bold text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] p-4",
       className
     )}
     {...props} />
@@ -59,12 +59,13 @@ const TableCell = React.forwardRef(({ className, ...props }, ref) => (
   <td
     ref={ref}
     className={cn(
-      "p-2 align-middle border-r [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] p-4",
+      "p-2 align-middle border-r [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] py-4 w-[100px] text-center", 
       className
     )}
     {...props} />
 ))
 TableCell.displayName = "TableCell"
+
 
 const TableCaption = React.forwardRef(({ className, ...props }, ref) => (
   <caption

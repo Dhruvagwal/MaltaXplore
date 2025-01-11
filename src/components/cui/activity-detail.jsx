861b0f7bb4cee7eb4160baa71d8426eac5058fd9
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-11 12:03:59 +0530

    added address fields

--- src/components/cui/activity-detail.jsx
+++ src/components/cui/activity-detail.jsx
@@ -1,18 +1,17 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import useCustomForm from "@/hooks/use-custom-form";
 import { Button } from "@/components/ui/button";
 import { maltaLocations } from "@/data/data";
 import { ChevronRight } from "lucide-react";
 import { activityDetailsSchema } from "@/lib/schema";
-import { useContactDetails } from "@/context/contactDetailsContext";
+import { useAddress } from "@/context/addressContext";
 import { useToast } from "@/hooks/use-toast";
 
 const ActivityDetailsPage = ({ nextStep }) => {
-    const { toast } = useToast();
-  
-  const { fname, setFname, lname, setLname, setPickupLocation } =
-    useContactDetails();
+  const { toast } = useToast();
+  const { setPickupLocation, setStreet, setCity, setState, setPostalCode } =
+    useAddress();
   const {
     FormWrapper,
     FormInput,
@@ -23,9 +22,11 @@ const ActivityDetailsPage = ({ nextStep }) => {
   });
 
   const handleSubmit = async (data) => {
-    setFname(data.fname || fname);
-    setLname(data.lname || lname);
     setPickupLocation(data.location);
+    setStreet(data.street);
+    setCity(data.city);
+    setState(data.state);
+    setPostalCode(data.postalCode);
     nextStep();
   };
 
@@ -41,32 +42,38 @@ const ActivityDetailsPage = ({ nextStep }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="col-span-2">
-        <p className="text-3xl font-semibold my-8">Activity Details</p>
+        <p className="text-3xl font-semibold my-8">Address Details</p>
 
-        <div className="my-6">
-          <p className="col-span-2 text-base font-medium my-1">
-            Lead Traveller
-          </p>
+        <div className="">
           <FormWrapper
             className="flex flex-col gap-6"
             onSubmit={handleSubmit}
             onError={onError}
           >
+            <div className="col-span-2">
+              <FormSelect
+                id="location"
+                options={maltaLocations}
+                title="Pickup Location"
+                placeholder="Select a location"
+                className="bg-white h-12"
+                required
+              />
+            </div>
+
             <div className="grid grid-cols-2 gap-4">
               {[
                 {
-                  id: "fname",
-                  label: "First Name",
-                  placeholder: "Type your first name",
+                  id: "street",
+                  label: "Street Address",
+                  placeholder: "Type your Street name",
                   type: "text",
-                  value: fname,
                 },
                 {
-                  id: "lname",
-                  label: "Last Name",
-                  placeholder: "Type your last name",
+                  id: "city",
+                  label: "City/Town",
+                  placeholder: "Type your city name",
                   type: "text",
-                  value: lname,
                 },
               ].map((input) => (
                 <FormInput
@@ -80,19 +87,30 @@ const ActivityDetailsPage = ({ nextStep }) => {
               ))}
             </div>
 
-            <div className="col-span-2 my-6">
-              {/* <p className="font-medium text-sm mt-2">
-                The provider offers pickup from select locations.
-              </p> */}
-
-              <FormSelect
-                id="location"
-                options={maltaLocations}
-                title="Pickup Location"
-                placeholder="Select a location"
-                className="bg-white h-12"
-                required
-              />
+            <div className="grid grid-cols-2 gap-4">
+              {[
+                {
+                  id: "state",
+                  label: "State",
+                  placeholder: "Type your state name",
+                  type: "text",
+                },
+                {
+                  id: "postalCode",
+                  label: "Zip/Postal Code",
+                  placeholder: "Type your last name",
+                  type: "text",
+                },
+              ].map((input) => (
+                <FormInput
+                  id={input.id}
+                  title={input.label}
+                  placeholder={input.placeholder}
+                  required
+                  value={input.value}
+                  className="h-12"
+                />
+              ))}
             </div>
 
             <div className="my-8 flex justify-center">

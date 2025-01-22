c0ae9e7fd7095fe7e55e7dc4e278370e9a9cf003
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-22 15:13:32 +0530

    added dashboard

---
+++ src/pages/user/dashboard.js
@@ -0,0 +1,119 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import FavoriteEventComponent from "@/components/cui/favorite-trip";
+import BookingTableComponent from "@/components/cui/bookings-table";
+import BoookingsCard from "@/components/cui/bookings-card";
+import { useAuthState } from "@/context/ueAuthContext";
+import UserWrapper from "./_app";
+import { getUpcomingServices } from "@/features/dashboard/getUpcomingServices";
+import { getPastServices } from "@/features/dashboard/getPastServices";
+import { getServices } from "@/features/getServices";
+import { getUserLikes } from "@/features/getUserLikes";
+
+const Dashboard = () => {
+  const { user } = useAuthState();
+  const [userLikeServices, setUserLikeServices] = useState([]);
+  const [likes, setLikes] = useState();
+  const [pastBookings, setPastBookings] = useState([]);
+  const [upcomingBookings, setUpcomingBookings] = useState([]);
+
+  useEffect(() => {
+    const fetchedBookings = async () => {
+      try {
+        if (user) {
+          const upcomingServices = await getUpcomingServices(user?.id);
+          const pastServices = await getPastServices(user?.id);
+
+          setUpcomingBookings(upcomingServices);
+          setPastBookings(pastServices);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchedBookings();
+  }, [user]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedServices = await getServices();
+      if (fetchedServices && user?.id) {
+        const likesData = await getUserLikes(user.id);
+
+        if (likesData.length > 0) {
+          setLikes(likesData);
+          const userLikes = fetchedServices.filter((service) =>
+            likesData.some((like) => like.service_id === service.id)
+          );
+          setUserLikeServices(userLikes);
+        } else {
+          console.log("No likes data found for the user");
+        }
+      }
+    };
+
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
+
+  return (
+    <UserWrapper>
+      <div className="">
+        <h1 className="text-2xl md:text-3xl font-semibold">
+          All Booking Update
+        </h1>
+        <br />
+        {}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {/* Rectangle Card */}
+          <BoookingsCard
+            count={pastBookings?.length + upcomingBookings?.length}
+            heading={"Total Bookings"}
+          />
+          <BoookingsCard
+            count={upcomingBookings?.length}
+            heading={"Upcoming Bookings"}
+          />
+          <BoookingsCard
+            count={pastBookings?.length}
+            heading={"Past Bookings"}
+          />
+        </div>
+        <div className="mt-16">
+          <h1 className="text-2xl md:text-3xl font-semibold">
+            Upcoming & Past Booking
+          </h1>
+          <br />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <BookingTableComponent
+              heading={"Upcoming Bookings"}
+              data={upcomingBookings}
+            />
+            <BookingTableComponent
+              heading={"Past Bookings"}
+              data={pastBookings}
+            />
+          </div>
+        </div>
+        {userLikeServices?.length > 0 && (
+          <div className="mt-16">
+            <h1 className="text-2xl md:text-3xl font-semibold">
+              Your favorite Trip
+            </h1>
+            <br />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+              {userLikeServices?.map((item, index) => (
+                <FavoriteEventComponent key={index} data={item} likes={likes} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </UserWrapper>
+  );
+};
+
+export default Dashboard;

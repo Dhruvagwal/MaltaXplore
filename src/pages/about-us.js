a3baad1eb4d5c406dbd551b8c933302ac938ac0c
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-22 15:17:48 +0530

    changes in about us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,8 +1,8 @@
 import Section from "@/components/ui/Section";
-
-import { LinkedinIcon, TwitterIcon, InstagramIcon } from "lucide-react";
 import Banner from "@/components/cui/banner";
 import ContactForm from "@/components/cui/contactForm";
+import { LinkedinIcon, TwitterIcon, InstagramIcon } from "lucide-react";
+import Tilt from "react-parallax-tilt";
 
 const teamMembers = [
   {
@@ -49,7 +49,7 @@ const Aboutus = () => {
       <main>
         {/* Hero Banner */}
         <div className="">
-          <Banner url="https://s3-alpha-sig.figma.com/img/aed9/11f9/5766b3ca66f433c1089322fba55fa4ea?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dmoa03QWtuJMoyDk6in8fyx6l1yxVLvaOCYgqXvb7CJXoFMTyxu67fdybruH8nhBcRywIPFGBNsEPHw9iEqB0wqN-EBiN2FGukb7DvahC~XCPPYVAaSpI~QdizQpmmPaYaZEX-0NUanvoU1foIdxXgR0ORQeDbXDqv~UI9YxIf7B6piu6n62SwIGEiegNy7dxfE9e9CJO~lo5DSvFDHRch7Xw~8wd8wOQcgkdfctO0e1M1uyZD3hJevz6gXWaaGOqgiO1cmzL4ykDFWdPAz3pn-zYOTxWtV2h44T0z3J01bILGAHur9Y2MAO4LCeUE1GgJ8x-eSJ~-rzFXGvc0t4aA__">
+          <Banner url="/about-us-banner.jpg">
             <h1 className="text-3xl md:text-4xl md:text-5xl lg:text-6xl font-bold text-white">
               About Us
             </h1>
@@ -58,8 +58,7 @@ const Aboutus = () => {
 
         {/* Welcome Section */}
         <section className="relative py-12 md:py-24">
-          {/* <div className="container mx-auto px-4 flex flex-col lg:flex-row"> */}
-          <div className="mx-8 md:mx-32 md:px-4 flex flex-col lg:flex-row lg:justify-center lg:items-center gap-2 ">
+          <div className="mx-8 md:mx-20 max-md:px-4 flex flex-col lg:flex-row lg:justify-center lg:items-center gap-2 ">
             {/* Left content */}
             <div className="w-full lg:w-1/2 md:pr-8 md:mb-28">
               <h1 className="text-3xl md:text-4xl md:text-5xl font-bold mb-6">
@@ -84,32 +83,36 @@ const Aboutus = () => {
             </div>
 
             {/* Right images grid */}
+
             <div className="w-full lg:w-1/2 relative h-[600px] mt-16">
               {/* Top right image */}
               <div className="absolute right-0 top-0 w-[80%] h-[80%] md:w-[70%] md:h-[90%] rounded-2xl overflow-hidden z-20">
                 <img
-                  src="https://s3-alpha-sig.figma.com/img/05ed/4852/698f5c735a490086d2e974a69457a819?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QnbzC-WUrsIkV-TyHCI0Ez6pNcZqXYwxDuv2vNLtaNzITqJ-YxRK7Cmf7fgSToK3jmEMVnWOkrfKzIa3pTTegyQHv3inNUtrPPHhEcyznPwENO8b-jB7Zy4QCV0kI9YkFbPHWfi03Zo6AGFTtv7-ZtwE~eYhvxA0tIHGyR79mFGYeRlvAUQCyWenodtzdZaNf5jCbxCrhJsoqoaj8sB-mxWbk5G9LPuHkj6LoPVa43aqiI~dH2xu8nRlw06DGkfZiq7-9tbzD87AYf5UT8VGGWwBDSELODaG04Zfb3lB1m~xr4vy9vvLWtfX3T6McSVyZvtLMz6XV~6ts6M1up6Lcg__"
+                  src="/malta-arial-view.png"
                   alt="Malta aerial view"
                   className="w-full h-full object-cover"
                 />
               </div>
-
               {/* Top left floating image */}
               <div className="absolute left-0 md:left-16 top-[10%] w-[45%] h-[40%] md:w-[40%] md:h-[30%] rounded-2xl overflow-hidden z-30 shadow-xl bg-white p-2">
-                <img
-                  src="https://s3-alpha-sig.figma.com/img/7c56/b84f/3f11707386056417983e788ae19c9be0?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=q9kIs6TYU0d0kB0kKonJoL-EM4qLQG5b04hhAPxqNBP-~RCgvTj~mQpw3c2t2ccP5ehfmWDBlIECgKSVR-8JQ9x1GH~FeGPzzPUFwkvPq0CX52dahwl4SQQP9DOU5JFvIunwMx8YfAY6vCyzLRSzlkGuMN-TS2WAvjF49mwBW7Mx6xJMreGQtyozqEkWfhph9o~ApYP8f0hXew3u~Vdty8TmLwPbVpFStowWhR2yLvAjtZvRHMspM~3BMBNhXWGoOKOajY-jqxOF-4mhGjTNaxloqAgkw64Mx1OJlHtY2-d9ofLdxmXWLRrgDb-z3d9PIDbfX0M00TTcPMBswxaWxg__"
-                  alt="Malta church"
-                  className="w-full h-full object-cover rounded-xl"
-                />
+                <Tilt>
+                  <img
+                    src="/malta-church.png"
+                    alt="Malta church"
+                    className="w-full h-full object-cover rounded-xl"
+                  />
+                </Tilt>
               </div>
 
               {/* Bottom left floating image */}
               <div className="absolute left-[10%] md:left-[0%] bottom-[10%] md:bottom-[18%] w-[45%] h-[40%] rounded-2xl overflow-hidden z-30 shadow-xl bg-white p-2">
-                <img
-                  src="https://s3-alpha-sig.figma.com/img/6eb3/2fe2/8d42b38ec73af2fce03a4c3f84e7e8c3?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=k4RbZwgzU~5nI8CLG94VzQqCAmKQzsT~riFx0OmyiL22jDDLHTZO8kUFy2KjrB2qY7L~0iu2cQvrw3irFb-hoLPFa8nREiZRI6bdQj-xhgtt88PRMw7lnGC5a9ppbSLSseL6aob5cp0Kotrl0yPipEXF7Of9V99bkIUqLY3mT9At7knw-J1XEYTwRgFy21ko8DsFB83rQmFOlpK1gXcFhxNdMRX0V2X7gEPVFg8~cINifD8AT9f87x5Ekkj7J-4Iaa0BAJnorSslklI-t6M6bkKY58rCfgYK6yOiWWm0-yw9WoOUYTSC4nk5hn8y3OhWONOeIiy-M2oxpFLBxP-kRQ__"
-                  alt="Malta harbor"
-                  className="w-full h-full object-cover rounded-xl"
-                />
+                <Tilt>
+                  <img
+                    src="malta-harbor.png"
+                    alt="Malta harbor"
+                    className="w-full h-full object-cover rounded-xl"
+                  />
+                </Tilt>
               </div>
             </div>
           </div>
@@ -118,7 +121,7 @@ const Aboutus = () => {
         {/* Our Story Section */}
         <section className="relative py-12 md:py-24 bg-gray-50">
           {/* <div className="container mx-auto px-4"> */}
-          <div className="mx-auto md:mx-32 px-4">
+          <div className="mx-auto md:mx-20 max-md:px-4">
             <div className="flex flex-col lg:flex-row items-start gap-8">
               {/* Left side with image */}
               <div className="w-full lg:w-1/2 relative">
@@ -158,7 +161,7 @@ const Aboutus = () => {
         {/* CEO Section */}
         <section className="max-md:py-12 md:pt-24 relative">
           {/* <div className="container mx-auto px-4"> */}
-          <div className="mx-2 md:mx-32 px-4">
+          <div className="mx-2 md:mx-20 max-md:px-4">
             <div className="flex flex-col items-center text-center max-w-4xl mx-auto md:mb-24">
               <div className="flex items-center gap-4 mb-8">
                 <span className="text-[#E5484D] text-6xl font-serif">"</span>
@@ -166,14 +169,14 @@ const Aboutus = () => {
                   At MaltaXplore, we aim to inspire and guide travelers,
                   ensuring every visit to Malta is unforgettable and authentic.
                 </h2>
-                <span className="text-[#E5484D] text-6xl font-serif rotate-180">
+                <span className="text-[#E5484D] text-6xl font-serif rotate-180 mb-12">
                   "
                 </span>
               </div>
 
               <div className="flex flex-col items-center">
                 <img
-                  src="https://s3-alpha-sig.figma.com/img/b040/a8ec/478885ddff72c629f05c598763239495?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=D9PiKDYgt0RC5uRxZ7c36IHbGhFEYCxCgvwWAhxKLRA4CLO0Steuo9PEY8nHNDxZ3xg72Mwi5B505wvxvBlq096JL33JyQ-Oe2yv1ImYgzSUg~uJ9AuTPdAmNe4hY8XetJoDpnK7-IznmU-eiqltGQ~TvyFdJW5nZaGXMoOyJf1edw5OyckP14oF-nz0QQsCDLd-iMKmGV9E5sGvluCLwFA0e~jEIaAmGLIe8vUHPCBfWq8huRr8B-QfFl8pjN03de~iW4xNUuYEXtUAY9BxAGcyWH0BtQY5J2YaLnNZPeXoRlKNVlc7pY343H0L7EQq0QylxqsLUYQpc1ca8o-0MA__"
+                  src="/founder.png"
                   alt="Cleven D'amato"
                   className="w-24 h-24 rounded-full object-cover mb-4"
                 />
@@ -185,8 +188,8 @@ const Aboutus = () => {
         </section>
 
         {/* Our Mission Section */}
-        <section className="relative py-12 md:py-32">
-          <div className="relative max-w-[1400px] mx-8 md:mx-auto md:px-4">
+        <section className="relative py-12 md:py-16">
+          <div className="relative max-w-[1400px] mx-8 md:mx-auto md:px-6">
             {/* Background Image */}
             <div className="w-full h-[260px] md:h-[500px] rounded-[2rem] overflow-hidden">
               <img
@@ -207,32 +210,37 @@ const Aboutus = () => {
             <div className="max-w-6xl mx-auto md:px-4">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-24 -mt-32 relative z-10">
                 {/* Mission Card */}
-                <div className="bg-white p-8 rounded-2xl shadow-lg">
-                  <h3 className="text-2xl md:text-3xl font-bold mb-6">
-                    Our Mission
-                  </h3>
-                  <p className="text-gray-600 leading-relaxed">
-                    At MaltaXplore, our mission is to inspire and empower
-                    travelers by providing reliable, in-depth information about
-                    Malta's attractions, activities, and accommodations. We are
-                    committed to being your trusted resource, ensuring every
-                    visitor to Malta has access to accurate, up-to-date, and
-                    personalized travel insights.
-                  </p>
-                </div>
+                <Tilt>
+                  <div className="bg-white p-8 rounded-2xl shadow-lg">
+                    <h3 className="text-2xl md:text-3xl font-bold mb-6">
+                      Our Mission
+                    </h3>
+                    <p className="text-gray-600 leading-relaxed">
+                      At MaltaXplore, our mission is to inspire and empower
+                      travelers by providing reliable, in-depth information
+                      about Malta's attractions, activities, and accommodations.
+                      We are committed to being your trusted resource, ensuring
+                      every visitor to Malta has access to accurate, up-to-date,
+                      and personalized travel insights.
+                    </p>
+                  </div>
+                </Tilt>
 
                 {/* Vision Card */}
-                <div className="bg-white p-8 rounded-2xl shadow-lg">
-                  <h3 className="text-2xl md:text-3xl font-bold mb-6">
-                    Our Vision
-                  </h3>
-                  <p className="text-gray-600 leading-relaxed">
-                    To become the leading platform for exploring Malta,
-                    recognized globally for fostering meaningful travel
-                    experiences, promoting sustainable tourism, and celebrating
-                    the unique beauty of the Maltese Islands.
-                  </p>
-                </div>
+
+                <Tilt>
+                  <div className="bg-white p-8 rounded-2xl shadow-lg h-full">
+                    <h3 className="text-2xl md:text-3xl font-bold mb-6">
+                      Our Vision
+                    </h3>
+                    <p className="text-gray-600 leading-relaxed">
+                      To become the leading platform for exploring Malta,
+                      recognized globally for fostering meaningful travel
+                      experiences, promoting sustainable tourism, and
+                      celebrating the unique beauty of the Maltese Islands.
+                    </p>
+                  </div>
+                </Tilt>
               </div>
             </div>
           </div>
@@ -291,8 +299,8 @@ const Aboutus = () => {
 
         {/* Contact Section */}
         <Section className="bg-gray-50">
-          <div className="text-center mb-12">
-            <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
+          <div className="text-center">
+            <h2 className="text-3xl md:text-4xl font-bold text-gray-900 d">
               Contact Us
             </h2>
             <p className="text-lg text-gray-600 max-w-2xl mx-8 md:mx-auto">

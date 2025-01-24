b47bbef3427c0bfb1ead951fa8c7a0d181c4be3f
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-24 14:19:15 +0530

    added components

---
+++ src/components/Home/listing-everyday.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+export const ListingEveryDay = ({ className }) => {
+    return (
+      <Card className={`rounded-full md:w-96 h-24 ${className}`}>
+        <CardContent className="flex items-center justify-center gap-4 pt-0 p-4">
+            <Avatar className="">
+              <AvatarImage
+                src={`https://picsum.photos/600/400?random=${0}`}
+                alt={`Random image`}
+                className="rounded-full w-16 h-16"
+              />
+              <AvatarFallback className="bg-gray-200 text-gray-700">
+                Avatar
+              </AvatarFallback>
+            </Avatar>
+  
+          <div>
+            <p className="font-semibold text-base leading-tight">
+              200+ New <br />
+              Listings Everyday!
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  };
+  

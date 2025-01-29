98eae947b4d97f67e6d86bda3a36d44dec276a64
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-29 18:03:48 +0530

    added cancelled booking page

---
+++ src/pages/user/cancelled-bookings.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import UserWrapper from "./_app";
+import { getPastUpcomingBookings } from "@/features/dashboard/getPastUpcomingBookings.js";
+import { useAuthState } from "@/context/ueAuthContext";
+import { getServices } from "@/features/getServices";
+import { getUserLikes } from "@/features/getUserLikes";
+import BookingCardComponentNew from "@/components/cui/booking-card-new";
+
+const CancelledBookings = () => {
+  const { user } = useAuthState();
+  const [likes, setLikes] = useState();
+  const [cancelledBookings, setCancelledBookings] = useState([]);
+
+  useEffect(() => {
+    const fetchedBookings = async () => {
+      try {
+        if (user) {
+          const upcomingServices = await getPastUpcomingBookings(user?.id);
+          setCancelledBookings(upcomingServices.cancelledBookings);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchedBookings();
+  }, [user, likes]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedServices = await getServices();
+      if (fetchedServices && user?.id) {
+        const likesData = await getUserLikes(user.id);
+        setLikes(likesData);
+      }
+    };
+
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
+
+
+  return (
+    <UserWrapper>
+      <div>
+        {" "}
+        <h1 className="text-2xl md:text-3xl font-semibold">Upcoming Booking</h1>
+        <br />
+        <div className="flex flex-wrap gap-4 max-md:justify-center">
+          {cancelledBookings?.map((item, index) => (
+            <BookingCardComponentNew
+              key={index}
+              data={item?.services}
+              likes={likes}
+              bookingsData={item?.servicebookings}
+            />
+          ))}
+        </div>
+      </div>
+    </UserWrapper>
+  );
+};
+
+export default CancelledBookings;

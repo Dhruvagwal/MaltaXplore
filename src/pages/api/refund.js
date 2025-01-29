ec25082b74d50688b104aed402187ec95baf63c8
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-29 18:02:05 +0530

    added refund api

---
+++ src/pages/api/refund.js
@@ -0,0 +1,29 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+      const { paymentIntentId } = req.body;
+      console.log("Received paymentIntentId:", paymentIntentId);
+  
+      if (!paymentIntentId) {
+        return res.status(400).json({ error: "paymentIntentId is required" });
+      }
+  
+      try {
+        // Refund the full amount
+        const refund = await stripe.refunds.create({
+          payment_intent: paymentIntentId,
+        });
+        console.log("Refund response:", refund);
+        res.status(200).json(refund);
+      } catch (error) {
+        console.error("Error during refund:", error);
+        res.status(500).json({ error: error });
+      }
+    } else {
+      res.status(405).json({ error: "Method Not Allowed" });
+    }
+  }
+  

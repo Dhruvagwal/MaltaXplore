225005e0d32244a1733456c0a5c6989ddc1f2311
pratikmahendra <pratik.deepak.mahendra@sheetsway.com>
2025-01-31 11:57:20 +0530

    changes

--- src/pages/tour-listing/[id].js
+++ src/pages/tour-listing/[id].js
@@ -114,14 +114,24 @@ function TourismPage() {
 
   const handleDateChange = (selectedDate) => {
     const formattedDate = new Date(selectedDate);
-    const formattedDateString = formattedDate.toISOString().split("T")[0];
+
+    const year = formattedDate.getFullYear();
+    const month = String(formattedDate.getMonth() + 1).padStart(2, "0");
+    const day = String(formattedDate.getDate()).padStart(2, "0");
+
+    const formattedDateString = `${year}-${month}-${day}`;
 
     setDate(formattedDateString);
   };
 
   const handleEndDateChange = (selectedDate) => {
     const formattedDate = new Date(selectedDate);
-    const formattedDateString = formattedDate.toISOString().split("T")[0];
+
+    const year = formattedDate.getFullYear();
+    const month = String(formattedDate.getMonth() + 1).padStart(2, "0");
+    const day = String(formattedDate.getDate()).padStart(2, "0");
+
+    const formattedDateString = `${year}-${month}-${day}`;
 
     setEndDate(formattedDateString);
   };

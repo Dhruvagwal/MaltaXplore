2b418561deb5a8a27a1781400f741d84714fa5bc
dhruv aggarwal <dhruvgwal@gmail.com>
2025-02-04 02:07:51 +0530

    starter

--- src/pages/search.js
+++ src/pages/search.js
@@ -58,8 +58,6 @@ function ExploreCategories() {
   const [filteredData, setFilteredData] = useState([]);
   const [checkedServiceTypeIds, setCheckedServiceTypeIds] = useState([]);
   const [serviceSubType, setServiceSubType] = useState([]);
-
-console.log(services)
   const range = watch("range");
   const min = watch("min");
   const max = watch("max");
@@ -129,7 +127,6 @@ console.log(services)
         }
 
         if (formattedDate) {
-          console.log("formattedDate", formattedDate);
           queryBuilder = queryBuilder.eq("date", formattedDate);
         }
 
@@ -157,8 +154,6 @@ console.log(services)
 
   // Split the data into chunks (4 items per page)
   const chunkedData = chunkArray(filteredData, SIZE);
-  console.log("filteredData", filteredData)
-
   useEffect(() => {
     if (currentPage >= chunkedData.length) {
       handlePageChange(chunkedData.length - 1);
@@ -200,8 +195,6 @@ console.log(services)
       if (selectedIds.length > 0) {
         queryBuilder = queryBuilder.in("service_type", selectedIds);
       }
-
-      console.log("selectedIds", selectedIds);
 
       const { data: filterData, error } = await queryBuilder;
 
@@ -261,7 +254,7 @@ console.log(services)
                     {" "}
                     {serviceSubType?.map((subCat) => (
                       <div key={subCat.id} className="flex flex-col gap-4">
-                        <FormCheckbox id={subCat.id} title={subCat.name} />
+                        <FormCheckbox id={`sub.${subCat.id}`} title={subCat.name} />
                       </div>
                     ))}
                   </div>
@@ -272,30 +265,10 @@ console.log(services)
                   <Separator />
                   {[5, 4, 3, 2, 1].map((key) => (
                     <FormCheckbox
-                      id={String(key)}
+                      id={`ratings.${key}`}
                       title={<CommentRatings rating={key} name="ratings" />}
                     />
                   ))}
-                </div>
-                <div className="flex bg-primary-foreground p-4 rounded-lg flex-col gap-4">
-                  <p className="font-bold text-xl">Filter Price</p>
-                  <Separator />
-                  <FormSlider id="range" min={10} max={500} />
-                  <div className="flex items-center gap-4">
-                    <FormInput
-                      className={"bg-white"}
-                      placeholder="Minimum"
-                      type="number"
-                      id="min"
-                    />
-                    <Separator className="w-10 pt-1 rounded-full bg-primary" />
-                    <FormInput
-                      className={"bg-white"}
-                      placeholder="Maximum"
-                      type="number"
-                      id="max"
-                    />
-                  </div>
                 </div>
                 <div className="flex bg-primary-foreground p-4 rounded-lg flex-col gap-4">
                   <p className="font-bold text-xl">Select Location</p>
